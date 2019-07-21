4c415bf9c8fa3716cf976300124b1e39377463b1
Mark Hartmann <mark-hartmann@mstone.de>
2019-07-21 13:33:40 +0200

    created fixtures for PHPUnit

---
+++ tests/Fixtures/BaseDummy.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Hartmann\PropertyInfo\Tests\Fixtures;
+
+/**
+ * Class BaseDummy
+ *
+ * @package Hartmann\PropertyInfo\Tests\Fixtures
+ */
+class BaseDummy
+{
+    /**
+     * @var string
+     */
+    protected $name;
+
+    /**
+     * @var \DateTime|null
+     */
+    protected $updatedAt;
+}
---
+++ tests/Fixtures/Dummy.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Hartmann\PropertyInfo\Tests\Fixtures;
+
+/**
+ * Class Dummy
+ *
+ * @package Hartmann\PropertyInfo\Tests\Fixtures
+ *
+ * @property string        $description
+ * @property-read string[] $tags Array with tags
+ * @property-write int     $foo
+ */
+class Dummy extends BaseDummy
+{
+
+}
